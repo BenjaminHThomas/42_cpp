9f9f1c01cb94aa30d50d5994f28474a99a20d09f
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-08-29 18:05:45 +0200

    started adding int and float constructor

--- cpp_2/ex01/Fixed.cpp
+++ cpp_2/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 16:16:50 by bthomas           #+#    #+#             */
-/*   Updated: 2024/08/29 17:29:25 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/08/29 18:05:08 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,27 @@
 
 Fixed::Fixed() : fixed(0) {
 	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(int const n) {
+	int	lim = (1 << 23);
+	if (n > lim - 1 || n < -lim) { // need to think of a more elegant solution
+		std::cout << "int too big/small\n";
+		this->fixed = 0;
+		return ;
+	}
+	this->fixed = n << this->fractBits;
+	std::cout << "int constructor called\n";
+}
+
+Fixed::Fixed(float const f) {
+	if (f > (1 << 24) - 1 || f < -(1 << 24)) {
+		std::cout << "float too big/small\n";
+		this->fixed = 0;
+		return ;
+	}
+	this->fixed = f * 256;
+	std::cout << "float constructor called\n";
 }
 
 Fixed::~Fixed() {
--- cpp_2/ex01/Fixed.hpp
+++ cpp_2/ex01/Fixed.hpp
@@ -6,13 +6,14 @@
 /*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 16:16:28 by bthomas           #+#    #+#             */
-/*   Updated: 2024/08/29 16:53:06 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/08/29 17:38:53 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
@@ -21,11 +22,16 @@ class Fixed
 		static const int fractBits = 8;
 	public:
 		Fixed();
+		Fixed(int const n);
+		Fixed(float const f);
 		Fixed(const Fixed& other);
 		Fixed& operator=(const Fixed& other);
+		Fixed& operator<<(const Fixed& other);
 		~Fixed();
 		int getRawBits() const;
 		void setRawBits(int const raw);
+		float toFloat() const;
+		int toInt() const;
 };
 
 
