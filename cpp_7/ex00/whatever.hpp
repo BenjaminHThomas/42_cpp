9fee24fb881baa68a32f05dca1c63cc530605a34
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-09-28 18:18:07 +0200

    finished ex00

---
+++ cpp_7/ex00/whatever.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/28 18:00:23 by bthomas           #+#    #+#             */
+/*   Updated: 2024/09/28 18:17:47 by bthomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+template <typename T>
+void swap(T &val1, T &val2) {
+	T temp;
+	temp = val1;
+	val1 = val2;
+	val2 = temp;
+}
+
+template <typename T>
+T min(const T &val1, const T &val2) {
+	if (val1 < val2) {
+		return val1;
+	}
+	return val2;
+}
+
+template <typename T>
+T max(const T &val1, const T &val2) {
+	if (val1 > val2) {
+		return val1;
+	}
+	return val2;
+}
