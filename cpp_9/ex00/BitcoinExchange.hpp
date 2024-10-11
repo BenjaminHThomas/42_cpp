68e2f5a12e099466176a4bc5b873087c09e73d21
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-10-11 10:57:49 +0200

    started cpp9 ex00

--- cpp_9/ex00/BitcoinExchange.hpp
+++ cpp_9/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 12:40:32 by bthomas           #+#    #+#             */
-/*   Updated: 2024/10/10 12:40:38 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/10/10 12:56:05 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,24 @@
 # define BITCOINEXCHANGE_HPP
 
 #include <iostream>
+#include <map>
+
+struct Date {
+	int year;
+	int month;
+	int day;
+};
 
 class BitcoinExchange
 {
-private:
-protected:
-public:
-	BitcoinExchange ();
-	BitcoinExchange (const BitcoinExchange &bitcoinexchange);
-	~BitcoinExchange ();
-	BitcoinExchange & operator=(const BitcoinExchange &bitcoinexchange);
+	private:
+		std::map<Date, double> _btcPrices;
+
+	public:
+		BitcoinExchange ();
+		BitcoinExchange (const BitcoinExchange &other);
+		~BitcoinExchange ();
+		BitcoinExchange & operator=(const BitcoinExchange &other);
 };
 
 #endif /* BITCOINEXCHANGE_HPP */
